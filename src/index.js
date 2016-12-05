a97d2f4356b86c9157508ddaceadb051568938a3
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2016-12-05 15:59:51 +0100

    added some config files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-const foo = 'bar';
-foo.toUpperCase();
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hi</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('container')
+);
