b9a58190e6239d36d987fd402ecb85ed77e63c28
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2016-12-05 15:30:46 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const PATHS = {
+  app: './src/index.js',
+  html: './src/index.html',
+  dist: path.join(__dirname, 'dist')
+};
+
+module.exports = {
+  entry: {
+    javascript: PATHS.app,
+    html: PATHS.html
+  },
+  output: {
+    path: PATHS.dist,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: PATHS.dist
+  },
+  eslint: {
+    emitWarning: true
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.js?$/,
+        loaders: ["eslint-loader"],
+        exclude: /node_modules/
+      }
+    ],
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["react-hot", "babel-loader"]
+      }
+    ]
+  }
+};
